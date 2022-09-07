267c123ae65844caf0dcb18c08eb58c88dfbac54
unknown <sk3577311@gmail.com>
2022-09-07 16:48:31 +0530

    first

---
+++ src/views/LoginView.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-3">
+                <h2 class="text-center text-dark mt-5">Login Form</h2>
+                <div class="text-center mb-5 text-dark">Made with bootstrap</div>
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.btn-color {
+    background-color: #0e1c36;
+    color: #fff;
+
+}
+
+.profile-image-pic {
+    height: 200px;
+    width: 200px;
+    object-fit: cover;
+}
+
+
+
+.cardbody-color {
+    background-color: #ebf2fa;
+}
+
+a {
+    text-decoration: none;
+}
+</style>
